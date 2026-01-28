2ac1caaf9741544a7a8132797599dff6649967f0
pra24tyaksh <pratyakshsrivastava973@gmail.com>
2026-01-28 20:45:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (38.73%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -16,6 +16,8 @@ class Solution {
 public:
     vector<string> letterCombinations(string digits) {
         vector<string> ans;
+        if(digits.length()==0)
+        return ans;
         string output;
         int index=0;
         string mapping[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
