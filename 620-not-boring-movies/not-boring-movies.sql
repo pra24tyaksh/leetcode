02e9397b095784cb50895a56beaf7e34feb1c783
pra24tyaksh <pratyakshsrivastava973@gmail.com>
2026-04-03 00:27:12 +0530

    Time: 264 ms (35.11%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-SELECT *FROM Cinema
-WHERE id%2=1 AND description!='boring'
-ORDER BY rating DESC;
+select *from cinema
+where id%2!=0 and description !='boring'
+order by rating desc;
